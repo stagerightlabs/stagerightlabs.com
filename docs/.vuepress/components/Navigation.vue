359ad950f1a14c859268bb486ce80ff37d2ca66c
Ryan Durham <ryan@stagerightlabs.com>
2018-06-19 16:50:29 -0700

    Use semantic tags in navbar and connect blog link

--- docs/.vuepress/components/Navigation.vue
+++ docs/.vuepress/components/Navigation.vue
@@ -1,17 +1,22 @@
 <template>
-  <nav class="flex border-b-4 border-red justify-between items-center py-8">
+  <header class="flex border-b-4 border-red justify-between items-center py-8">
     <img src="../theme/images/logo-h.png" class="px-8" alt="Stage Right Labs logo">
-    <div>
-      <a href="#" class="text-black hover:text-red text-2xl mx-6">Blog</a>
+    <nav>
+      <a @click="goTo('/blog')" class="cursor-pointer text-black hover:text-red text-2xl mx-6">Blog</a>
       <a href="#" class="text-black hover:text-red text-2xl mx-6">Projects</a>
       <a href="#" class="text-black hover:text-red text-2xl mx-6">Contact</a>
-    </div>
-  </nav>
+    </nav>
+  </header>
 </template>
 
 <script>
 export default {
-
+  methods: {
+    goTo (path) {
+      console.log(path)
+      this.$router.push({ path })
+    }
+  }
 }
 </script>
 
