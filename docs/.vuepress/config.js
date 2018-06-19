39f5c9eff5369481155e9465e1541d2b4e3e2ed4
Ryan Durham <ryan@stagerightlabs.com>
2018-06-19 14:14:50 -0700

    Remove purge css

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,16 +1,4 @@
-const PurgecssPlugin = require("purgecss-webpack-plugin");
 const tailwindcss = require("tailwindcss");
-const glob = require("glob-all");
-const path = require("path");
-
-// Custom PurgeCSS extractor for Tailwind that allows special characters in
-// class names.
-//
-class TailwindExtractor {
-  static extract(content) {
-    return content.match(/[A-z0-9-:\/]+/g) || [];
-  }
-}
 
 module.exports = {
   title: "Stage Right Labs",
@@ -18,26 +6,6 @@ module.exports = {
     plugins: [
       tailwindcss("docs/.vuepress/tailwind.js"),
       require("autoprefixer")
-    ]
-  },
-  configureWebpack: {
-    plugins: [
-      new PurgecssPlugin({
-        // Specify the locations of any files you want to scan for class names.
-        paths: glob.sync([
-          path.join(__dirname, "./theme/**/*.vue"),
-          path.join(__dirname, "./components/**/*.vue")
-        ]),
-        extractors: [
-          {
-            extractor: TailwindExtractor,
-
-            // Specify the file extensions to include when scanning for
-            // class names.
-            extensions: ["vue"]
-          }
-        ]
-      })
     ]
   }
 };
