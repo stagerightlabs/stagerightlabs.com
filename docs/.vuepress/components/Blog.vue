6670800dfb1ef832c04aa16fc7bac10504c6d818
Ryan Durham <ryan@stagerightlabs.com>
2018-06-19 16:41:55 -0700

    Pagination and category filtering

--- docs/.vuepress/components/Blog.vue
+++ docs/.vuepress/components/Blog.vue
@@ -1,20 +1,32 @@
 <template>
   <main class="flex mt-8 mx-8">
     <section class="w-4/5">
-      <h2>Blog</h2>
-      <article v-for="post in posts" class="mt-8 mx-8">
+      <h2>
+        <span v-if="selectedCategory">Cateory: {{ selectedCategory }}</span>
+        <span v-else>Blog</span>
+      </h2>
+      <article v-for="post in paginatedPosts" class="mt-8 mx-8">
         <h2>
           <a :href="post.path">{{ post.title }}</a>
         </h2>
         <p class="mt-4" v-html="post.excerpt"></p>
       </article>
+
+      <div class="pagination my-8 flex justify-around" v-if="pageCount > 1">
+        <span>
+          <a @click="previousPage" :class="[paginationBackwardAllowed ? 'cursor-pointer' : 'cursor-not-allowed']">Newer</a>
+        </span>
+        <span>
+          <a @click=" nextPage " :class="[paginationForwardAllowed ? 'cursor-pointer' : 'cursor-not-allowed'] ">Older</a>
+        </span>
+      </div>
     </section>
 
-    <aside class="w-1/5 pl-4">
+    <aside class="w-1/5 pl-4 ">
       <h3>Topics</h3>
-      <ul class="mt-4 list-reset">
-        <li v-for="category in categories">
-          <a href="#">{{ category }}</a>
+      <ul class="mt-4 list-reset ">
+        <li v-for="category in categories ">
+          <a href="# ">{{ category }}</a>
         </li>
       </ul>
     </aside>
@@ -23,17 +35,77 @@
 
 <script>
 export default {
+  methods: {
+    nextPage () {
+      let nextPage = this.pageNumber + 1;
+
+      if (nextPage > this.pageCount) {
+        nextPage = this.pageCount
+      }
+
+      this.$router.push({ query: { page: nextPage } })
+    },
+
+    previousPage () {
+      let previousPage = this.pageNumber - 1;
+
+      if (previousPage < 1) {
+        previousPage = 1;
+      }
+
+      this.$router.push({ query: { page: previousPage } })
+    }
+  },
   computed: {
     posts () {
-      return this.$site.pages.filter(function (page) {
+      let posts = this.$site.pages.filter(function (page) {
         return page.frontmatter.layout === 'BlogPost'
       })
+        .sort((a, b) => a.frontmatter.date < b.frontmatter.date);
+
+      if (this.selectedCategory) {
+        console.log(this.selectedCategory)
+        posts = posts.filter((post) => {
+          return post.frontmatter.categories
+            .map((category) => category.toLowerCase())
+            .includes(this.selectedCategory.toLowerCase());
+        });
+      }
+
+      return posts;
+    },
+    paginatedPosts () {
+      const start = (this.pageNumber - 1) * 5;
+      const end = start + 5;
+      return this.posts.slice(start, end);
+    },
+    pageNumber () {
+      let pageNumber = this.$route.query.page || 1;
+      if (pageNumber > this.pageCount) {
+        pageNumber = this.pageCount;
+      }
+      if (pageNumber < 1) {
+        pageNumber = 1;
+      }
+      return pageNumber;
+    },
+    pageCount () {
+      return Math.floor(this.posts.length / 5);
+    },
+    paginationForwardAllowed () {
+      return this.pageNumber < this.pageCount;
+    },
+    paginationBackwardAllowed () {
+      return this.pageNumber > 1;
     },
     categories () {
       return this.$site.pages
         .reduce((categories, page) => categories.concat(page.frontmatter.categories), [])
         .filter((element, index, array) => array.indexOf(element) == index)
         .filter((category) => category);
+    },
+    selectedCategory () {
+      return this.$route.query.category || null
     }
   }
 }
