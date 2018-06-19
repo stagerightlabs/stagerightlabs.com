4fd5e5090f54712c5d52d061c2a9476c78707c92
Ryan Durham <ryan@stagerightlabs.com>
2018-06-19 11:23:17 -0700

    Initial commit

---
+++ .vuepress/theme/Layout.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="container mx-auto">
+    <Content/>
+  </div>
+</template>
+
+<style lang="scss">
+@import './theme.scss'
+</style>
---
+++ README.md
@@ -0,0 +1,3 @@
+# Hello VuePress
+
+This is some content.
