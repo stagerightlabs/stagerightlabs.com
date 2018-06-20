78068bf235c2e64199820b7694b110f92251fd59
Ryan Durham <ryan@stagerightlabs.com>
2018-06-19 17:03:44 -0700

    Add favicon

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,6 +2,7 @@ const tailwindcss = require("tailwindcss");
 
 module.exports = {
   title: "Stage Right Labs",
+  head: [["link", { rel: "icon", href: "/favicon-96x96.png" }]],
   postcss: {
     plugins: [
       tailwindcss("docs/.vuepress/tailwind.js"),
