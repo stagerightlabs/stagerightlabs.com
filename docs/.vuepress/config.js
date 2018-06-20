d8c4b5e4b511ce8a3f9ab044bd966a89da9f64dc
Ryan Durham <ryan@stagerightlabs.com>
2018-06-20 13:48:06 -0700

    Add matomo tracking script

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,7 +2,27 @@ const tailwindcss = require("tailwindcss");
 
 module.exports = {
   title: "Stage Right Labs",
-  head: [["link", { rel: "icon", href: "/favicon-96x96.png" }]],
+  head: [
+    // Favicon
+    ["link", { rel: "icon", href: "/favicon-96x96.png" }],
+    // Matomo tracking script
+    [
+      "script",
+      { type: "text/javascript" },
+      `
+  var _paq = _paq || [];
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u="//stats.stagerightlabs.com/";
+    _paq.push(['setTrackerUrl', u+'piwik.php']);
+    _paq.push(['setSiteId', '2']);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+  })();
+    `
+    ]
+  ],
   postcss: {
     plugins: [
       tailwindcss("docs/.vuepress/tailwind.js"),
