15d8a96187bcf11cb7c2a863f271ac604e65fcd9
Ryan Durham <ryan@stagerightlabs.com>
2018-06-22 14:27:14 -0700

    Enable eslint

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:vue/recommended"
+    ],
+    "parserOptions": {
+        "sourceType": "module"
+    }
+};
