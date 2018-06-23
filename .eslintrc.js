de69773b505a899b306afff51c3327aeadb83278
Ryan Durham <ryan@stagerightlabs.com>
2018-06-23 08:31:53 -0700

    Tell ESlint we are operating in a node environment

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,7 @@
 module.exports = {
     "env": {
         "browser": true,
+        "node": true,
         "es6": true
     },
     "extends": [
@@ -9,5 +10,5 @@ module.exports = {
     ],
     "parserOptions": {
         "sourceType": "module"
-    }
+    },
 };
