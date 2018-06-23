fe30aed1703addd9f080d6021e2e2132d4c04d56
Ryan Durham <ryan@stagerightlabs.com>
2018-06-23 08:04:28 -0700

    Update to vue-awesome 3.*

--- content/.vuepress/enhanceApp.js
+++ content/.vuepress/enhanceApp.js
@@ -1,15 +1,11 @@
-import Icon from "vue-awesome/components/Icon";
 import Layout from "./theme/Layout.vue";
 
 export default ({
-  Vue, // the version of Vue being used in the VuePress app
-  options, // the options for the root Vue instance
+  //Vue, // the version of Vue being used in the VuePress app
+  //options, // the options for the root Vue instance
   router, // the router instance for the app
-  siteData // site metadata
+  // siteData // site metadata
 }) => {
-  // Register third party plugins
-  Vue.component('icon', Icon);
-
   // Register URL Redirects
   // router.addRoutes([{ path: "/blog/", redirect: "/" }]);
   router.addRoutes([{ path: "/blog", component: Layout }]);
--- content/.vuepress/theme/Contact.vue
+++ content/.vuepress/theme/Contact.vue
@@ -3,19 +3,32 @@
     <h2 class="w-full my-8 text-black">Contact</h2>
     <section class="w-full flex flex-wrap">
       <div class="w-full md:w-1/3 mb-8">
-        <a href="https://github.com/srlabs" class="text-black hover:text-red flex flex-col items-center">
-          <icon name="github-alt" scale="7" class="mb-4"></icon>
+        <a
+          href="https://github.com/srlabs"
+          class="text-black hover:text-red flex flex-col items-center">
+          <icon
+            name="brands/github-alt"
+            scale="7"
+            class="mb-4"/>
           github
         </a>
       </div>
       <div class="w-full md:w-1/3 mb-8">
-        <a href="https://gitlab.com/stage-right-labs" class="text-black hover:text-red flex flex-col items-center">
-          <icon name="gitlab" scale="7" class="mb-4"></icon>
+        <a
+          href="https://gitlab.com/stage-right-labs"
+          class="text-black hover:text-red flex flex-col items-center">
+          <icon
+            name="brands/gitlab"
+            scale="7"
+            class="mb-4"/>
           gitlab
         </a>
       </div>
       <div class="w-full md:w-1/3 flex flex-col items-center mb-8 text-black">
-        <icon name="envelope-o" scale="7" class="mb-4"></icon>
+        <icon
+          name="regular/envelope"
+          scale="7"
+          class="mb-4"/>
         ryan at stagerightlabs dot com
       </div>
     </section>
@@ -23,10 +36,11 @@
 </template>
 
 <script>
-import 'vue-awesome/icons/envelope-o';
-import 'vue-awesome/icons/github-alt';
-import 'vue-awesome/icons/gitlab';
+import * as Icon from 'vue-awesome';
+import 'vue-awesome/icons/regular/envelope';
+import 'vue-awesome/icons/brands/github-alt';
+import 'vue-awesome/icons/brands/gitlab';
 export default {
-
+  components: { Icon }
 }
 </script>
