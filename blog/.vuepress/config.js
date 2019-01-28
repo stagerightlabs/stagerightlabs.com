f6e36e337239fcc8895c8a812257812bf90527ad
Ryan Durham <ryan@stagerightlabs.com>
2019-01-27 21:51:41 -0800

    Fix matomo analytics tracking code

--- blog/.vuepress/config.js
+++ blog/.vuepress/config.js
@@ -15,15 +15,15 @@ module.exports = {
       "script",
       { type: "text/javascript" },
       `
-  var _paq = _paq || [];
+  var _paq = window._paq || [];
   _paq.push(['trackPageView']);
   _paq.push(['enableLinkTracking']);
   (function() {
     var u="//stats.stagerightlabs.com/";
-    _paq.push(['setTrackerUrl', u+'piwik.php']);
+    _paq.push(['setTrackerUrl', u+'matomo.php']);
     _paq.push(['setSiteId', '2']);
     var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
   })();
     `
     ]
