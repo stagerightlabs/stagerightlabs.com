56f67d888af9922fab41361190932ccb940ffa96
Ryan C. Durham <ryan@stagerightlabs.com>
2020-08-27 16:56:35 -0700

    Configure user model and add account creation command

--- app/User.php
+++ app/User.php
@@ -2,22 +2,21 @@
 
 namespace App;
 
+use App\Concerns\UuidAsPrimaryKey;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmail
 {
-    use Notifiable;
+    use Notifiable, UuidAsPrimaryKey;
 
     /**
-     * The attributes that are mass assignable.
+     * The attributes that aren't mass assignable.
      *
      * @var array
      */
-    protected $fillable = [
-        'name', 'email', 'password',
-    ];
+    protected $guarded = [];
 
     /**
      * The attributes that should be hidden for arrays.
@@ -34,6 +33,19 @@ class User extends Authenticatable
      * @var array
      */
     protected $casts = [
+        'id' => 'string',
+        'last_login' => 'datetime',
+        'allowed_backstage' => 'boolean',
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * Is this user allowed backstage?
+     *
+     * @return bool
+     */
+    public function isAllowedBackstage()
+    {
+        return $this->allowed_backstage;
+    }
 }
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,11 +14,13 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->uuid('id')->primary();
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->timestamp('last_login')->nullable();
             $table->string('password');
+            $table->boolean('allowed_backstage')->default(false);
             $table->rememberToken();
             $table->timestamps();
         });
