ce9301e731b22d2e54170754177311a310b33339
Ryan C. Durham <ryan@stagerightlabs.com>
2020-09-04 12:21:51 -0700

    Set up page heading blade component

---
+++ resources/views/components/heading.blade.php
@@ -0,0 +1,17 @@
+<div {{ $attributes->merge(['class' => 'lg:flex lg:items-center lg:justify-between mb-8']) }}>
+  <div class="flex-1 min-w-0">
+    <h2 class="text-2xl font-bold leading-7 text-cool-gray-300 sm:text-3xl sm:leading-9 sm:truncate">
+      {{ $slot }}
+    </h2>
+    @isset($meta)
+    <div class="mt-1 flex flex-col sm:mt-0 sm:flex-row sm:flex-wrap">
+      {{ $meta }}
+    </div>
+    @endisset
+  </div>
+  @isset($options)
+  <div class="mt-5 flex lg:mt-0 lg:ml-4">
+    {{ $options }}
+  </div>
+  @endisset
+</div>
