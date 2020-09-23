36d23ebc3b89fcd1dc11742ed791df76eb2d7d9f
Ryan C. Durham <ryan@stagerightlabs.com>
2020-09-23 12:18:39 -0700

    Set up favicon assets

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -10,7 +10,10 @@
     @endif
 
     <!-- Favicon -->
-    <link rel="shortcut icon" href="{{ url(asset('favicon.ico')) }}">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ url(asset('icon/apple-touch-icon.png')) }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ url(asset('icon/favicon-32x32.png')) }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ url(asset('icon/favicon-16x16.png')) }}">
+    <link rel="manifest" href="{{ url(asset('icon/site.webmanifest')) }}">
 
     <!-- Fonts -->
     <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
