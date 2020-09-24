e16017280b4bd899188d84adb6aa0a773a7d3d26
Ryan C. Durham <ryan@stagerightlabs.com>
2020-09-24 11:41:17 -0700

    Set up "about" page

---
+++ app/Http/Livewire/About.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class About extends Component
+{
+    /**
+     * Render the component.
+     *
+     * @return \Illuminate\View\View
+     */
+    public function render()
+    {
+        return view('livewire.about');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::livewire('/', 'blog-index')->name('home');
 Route::livewire('blog/{slug}', 'blog-post')->name('blog.post');
+Route::livewire('about', 'about');
 
 Route::layout('layouts.auth')->group(function () {
     Route::middleware('guest')->group(function () {
