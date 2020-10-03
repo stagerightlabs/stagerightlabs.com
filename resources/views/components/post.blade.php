4c70c7ddbf39037fa06c5531223b8a60428849b6
Ryan C. Durham <ryan@stagerightlabs.com>
2020-10-03 13:52:37 -0700

    Set up Post blade component with its own view logic

---
+++ resources/views/components/post.blade.php
@@ -0,0 +1,29 @@
+<article class="post">
+  <header class="pb-4">
+    <h1 class="text-3xl mb-2">{{ $post->title }}</h1>
+    <div class="md:flex md:justify-between md:items-center">
+      @if ($post->hasBeenPublished())
+      <time
+        datetime="{{ $post->published_at->toAtomString() }}"
+        class="block mb-1 text-cool-gray-400">
+        {{ $post->published_at->format('F j, Y') }}
+      </time>
+      @else
+        <div class="mb-1 text-cool-gray-400">DRAFT</div>
+      @endif
+      <div>
+        @foreach($post->tags as $tag)
+          <x-tag>{{ $tag->name }}</x-tag>
+        @endforeach
+      </div>
+    </div>
+  </header>
+  @if($wasPublishedMoreThanAYearAgo)
+    <aside class="text-center text-cool-gray-400 p-4 mt-4 mb-8 bg-cool-gray-900 rounded-md">
+      Heads up! This article is more than {{ $publicationAgeForHumans }} the content may be out of date.
+    </aside>
+  @endif
+  <div class="content">
+    {!! $post->rendered !!}
+  </div>
+</article>
--- resources/views/livewire/backstage/post-preview.blade.php
+++ resources/views/livewire/backstage/post-preview.blade.php
@@ -14,35 +14,7 @@
   </x-card>
   @else
   <x-card>
-    <article class="post">
-      <header class="pb-4">
-        <h1 class="text-3xl mb-2">{{ $post->title }}</h1>
-        <div class="md:flex md:justify-between md:items-center">
-          @if ($post->hasBeenPublished())
-          <time
-            datetime="{{ $post->published_at->toAtomString() }}"
-            class="block mb-1 text-cool-gray-400">
-            {{ $post->published_at->format('F j, Y') }}
-          </time>
-          @else
-            <div class="mb-1 text-cool-gray-400">DRAFT</div>
-          @endif
-          <div>
-            @foreach($post->tags as $tag)
-              <x-tag>{{ $tag->name }}</x-tag>
-            @endforeach
-          </div>
-        </div>
-      </header>
-      @if($post->wasPublishedMoreThanAYearAgo())
-        <aside class="border border-red-800 text-center text-cool-gray-400 p-4 mt-4 mb-8">
-          This post is more than a year old; it is possible the content is now a bit out of date.
-        </aside>
-      @endif
-      <div class="content">
-        {!! $post->rendered !!}
-      </div>
-    </article>
+    <x-post :post="$post" />
   </x-card>
   @endif
 </div>
