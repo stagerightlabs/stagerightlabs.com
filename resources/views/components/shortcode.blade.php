f35898d411b83fb64f60b989c1a9248f41cf96d1
Ryan C. Durham <ryan@stagerightlabs.com>
2020-10-08 09:06:07 -0700

    Let the user copy the shortcode by clicking on it.

---
+++ resources/views/components/shortcode.blade.php
@@ -0,0 +1,27 @@
+@props([
+  'shortcode' => ''
+])
+
+@if ($shortcode)
+<div x-data="{copied: false}">
+  <code
+    @click="
+      copyToClipboard($event.target.getAttribute('data-shortcode'));
+      window.setTimeout(() => { copied = false;}, 500);
+      copied = true
+    "
+    class="cursor-pointer"
+    data-shortcode="{{ $shortcode }}"
+  >{{ $shortcode }}</code>
+  <span
+    x-show="copied"
+    class="text-cool-gray-500"
+    x-transition:enter="transition ease-out duration-75"
+    x-transition:enter-start="opacity-0 transform scale-90"
+    x-transition:enter-end="opacity-100 transform scale-100"
+    x-transition:leave="transition ease-in duration-100"
+    x-transition:leave-start="opacity-100 transform scale-100"
+    x-transition:leave-end="opacity-0 transform scale-90"
+  >copied</span>
+</div>
+@endif
