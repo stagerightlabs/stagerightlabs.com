3ab69b157624b3d4e4a0c91b280d49eaa68dacf4
Ryan C. Durham <ryan@stagerightlabs.com>
2020-12-14 10:39:59 -0800

    Use file extension routing to optionally display post in raw markdown.

---
+++ app/Http/Controllers/MarkdownController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Actions\Posts\PostMarkdownRenderingAction;
+use App\Post;
+use App\Utilities\Flash;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Response;
+
+class MarkdownController extends Controller
+{
+    public function show($slug)
+    {
+        $post = Post::published()
+            ->where('slug', $slug)
+            ->first();
+
+        if (!$post) {
+            abort(404);
+        }
+
+        $markdown = PostMarkdownRenderingAction::execute(['post' => $post]);
+
+        if ($markdown->failed()) {
+            Flash::error("There was a problem rendering markdown for that post.");
+        return redirect()->route('home');
+        }
+
+        // Return our markdown as plain text.
+        $response = Response::make($markdown->rendered, 200);
+        $response->header('Content-Type', 'text/plain');
+
+        return $response;
+    }
+}
