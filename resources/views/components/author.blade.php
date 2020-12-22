4ca3f0d37e109ce7b27d68c14bb75bbcc1e1cbdb
Ryan C. Durham <ryan@stagerightlabs.com>
2020-12-21 16:15:05 -0800

    Add sidebar to posts on large screens

--- resources/views/components/author.blade.php
+++ resources/views/components/author.blade.php
@@ -1,4 +1,4 @@
-<x-card class="mb-4 author">
+<x-card {{ $attributes->merge(['class' => 'mb-4 px-4 author'])}}>
   <div class="grid grid-cols-1 md:grid-cols-9 gap-4">
     <div class="col-span-1 md:col-span-3">
       <img src="{{ url(asset('img/avatar.jpg')) }}" title="Avatar for Ryan Durham" class="rounded-full w-64 mx-auto p-8" />
@@ -6,7 +6,7 @@
     <div class="col-span-1 md:col-span-6 flex justify-center flex-col">
       <h1 class="text-3xl mb-4">About the Author</h1>
       <p class="text-lg mb-4">
-        Ryan C. Durham is a software developer who lives in Portland, Oregon, with his wife and daughter. His numerous areas of interest include PHP, Laravel, Elixir and PostgreSQL, as well as organizational efficiency and communications strategies.
+        Ryan C. Durham is a software developer who lives in Portland, Oregon, with his wife and daughter. His numerous areas of interest include PHP, Laravel, Golang and PostgreSQL, as well as organizational efficiency and communications strategies.
       </p>
       <p class="text-lg">
         You can find him on <a href="https://github.com/stagerightlabs/" target="_blank" >GitHub</a>
--- resources/views/components/post.blade.php
+++ resources/views/components/post.blade.php
@@ -1,22 +1,4 @@
-<article class="post">
-  <header class="pb-4">
-    <div class="md:flex md:justify-between md:items-center">
-      @if ($post->hasBeenPublished())
-      <time
-        datetime="{{ $post->published_at->toAtomString() }}"
-        class="block mb-1 text-cool-gray-400">
-        {{ $post->published_at->format('F j, Y') }}
-      </time>
-      @else
-        <div class="mb-1 text-cool-gray-400">DRAFT</div>
-      @endif
-      <div>
-        @foreach($post->tags as $tag)
-          <x-tag>{{ $tag->name }}</x-tag>
-        @endforeach
-      </div>
-    </div>
-  </header>
+<article class="post my-4 mx-2">
   @if($wasPublishedMoreThanAYearAgo())
     <aside class="text-center text-cool-gray-400 p-4 mt-4 mb-4 bg-cool-gray-900 rounded-md">
       Heads up! This article is more than {{ $publicationAgeForHumans }} the content may be out of date.
