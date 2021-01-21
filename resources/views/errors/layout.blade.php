03ac03cb7a0181d3e08ceab961154b9a46e1e2ea
Ryan C. Durham <ryan@stagerightlabs.com>
2021-01-21 14:59:33 -0800

    Update error page template with new base layout

--- resources/views/errors/layout.blade.php
+++ resources/views/errors/layout.blade.php
@@ -1,4 +1,4 @@
-<x-layouts.base>
+<x-base-layout>
  <div class="relative flex items-top justify-center min-h-screen sm:items-center sm:pt-0">
     <div class="max-w-xl mx-auto sm:px-6 lg:px-8">
       <div class="flex items-center pt-8 sm:justify-start sm:pt-0">
@@ -10,4 +10,4 @@
       </div>
     </div>
   </div>
-</x-layouts.base>
+</x-base-layout>
