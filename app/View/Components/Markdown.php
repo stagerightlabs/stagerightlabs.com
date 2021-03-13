70fb5b321367fd7ad1f3c4d0faad2214d97a1eba
Ryan C. Durham <ryan@stagerightlabs.com>
2021-03-13 07:02:33 -0800

    Add markdown view component

---
+++ app/View/Components/Markdown.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\View\Components;
+
+use Illuminate\View\Component;
+use League\CommonMark\CommonMarkConverter;
+
+/**
+ * Inspired by the Blade UI Kit
+ * https://blade-ui-kit.com/docs/0.x/markdown
+ */
+class Markdown extends Component
+{
+    /**
+     * Get the view / contents that represent the component.
+     *
+     * @return \Illuminate\Contracts\View\View|string
+     */
+    public function render()
+    {
+        return view('components.markdown');
+    }
+
+    /**
+     * Convert markdown content to HTML.
+     *
+     * @param string $markdown
+     * @return string
+     */
+    public function toHtml(string $markdown)
+    {
+        return (new CommonMarkConverter())->convertToHtml($markdown);
+    }
+}
