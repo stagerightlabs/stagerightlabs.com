a743a18d4e1fdf05414d7a74cfd9c2e02085d991
Ryan C. Durham <ryan@stagerightlabs.com>
2021-03-18 08:56:17 -0700

    Implement custom sortable directive for Livewire

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,5 @@
-require('./bootstrap');
+import 'alpinejs';
+import Sortable from '@shopify/draggable/lib/sortable'
 
 // https://inspiredwebdev.com/copy-to-clipboard-with-javascript
 window.copyToClipboard = function(text) {
@@ -12,3 +13,50 @@ window.copyToClipboard = function(text) {
   document.execCommand('copy');
   document.body.removeChild(el);
 }
+
+// Custom livewire sortable directive based on https://github.com/livewire/sortable
+// Updated to adjust mirror configuration and add additional classes
+// based on drag state.
+window.livewire.directive('sortable', (el, directive, component) => {
+    // Only fire this handler on the "root" directive.
+    if (directive.modifiers.length > 0) return
+
+    let options = {
+      draggable: '[wire\\:sortable\\.item]',
+      classes: {
+        'draggable:over': ['draggable--over'],
+      }
+    }
+
+    // Should we constrain the dimensions of the mirror element?
+    if (el.hasAttribute('wire:sortable.mirror.constrain')) {
+        options.mirror = options.mirror || {}
+        options.mirror.constrainDimensions = true
+    }
+
+    // Have any classes been specified for the 'draggable:over' element?
+    if (el.getAttribute('wire:sortable.classes.over')) {
+      options.classes['draggable:over'] = options.classes['draggable:over'].concat(
+        el.getAttribute('wire:sortable.classes.over').split(' ')
+      )
+    }
+
+    // Define the draggable handle if it has been specified
+    if (el.querySelector('[wire\\:sortable\\.handle]')) {
+        options.handle ='[wire\\:sortable\\.handle]'
+    }
+
+    const sortable = new Sortable(el, options);
+
+    sortable.on('sortable:stop', () => {
+        setTimeout(() => {
+            let items = []
+
+            el.querySelectorAll('[wire\\:sortable\\.item]').forEach((el, index) => {
+                items.push({ order: index + 1, value: el.getAttribute('wire:sortable.item')})
+            })
+
+            component.call(directive.method, items)
+        }, 1)
+    })
+})
