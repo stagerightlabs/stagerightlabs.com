52869d3fc9b6aa4037f35e00f4242c01c9ad5164
Ryan C. Durham <ryan@stagerightlabs.com>
2022-12-27 19:41:35 -0800

    Remove the sentry error tracker

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -36,10 +36,6 @@ class Handler extends ExceptionHandler
      */
     public function report(Throwable $exception)
     {
-        if (app()->bound('sentry') && $this->shouldReport($exception)) {
-            app('sentry')->captureException($exception);
-        }
-
         parent::report($exception);
     }
 
