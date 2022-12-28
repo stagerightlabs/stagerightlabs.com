2d23ca0e5504a67d4442d701d001b6e708aab02f
Ryan C. Durham <ryan@stagerightlabs.com>
2022-12-27 19:44:30 -0800

    Set up honeybadger error tracking

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -36,6 +36,10 @@ class Handler extends ExceptionHandler
      */
     public function report(Throwable $exception)
     {
+        if (app()->bound('honeybadger') && $this->shouldReport($exception)) {
+            app('honeybadger')->notify($exception, app('request'));
+        }
+
         parent::report($exception);
     }
 
