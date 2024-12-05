c4a3732e7edc4f2ac8ab881a72b2e096b914c67d
Ryan C. Durham <ryan@stagerightlabs.com>
2024-12-05 09:26:36 -0800

    Configure tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,31 +1,11 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
-
-module.exports = {
+/** @type {import('tailwindcss').Config} */
+export default {
   content: [
-    './app/**/*.php',
-    './resources/**/*.html',
-    './resources/**/*.js',
-    './resources/**/*.jsx',
-    './resources/**/*.php'
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
   ],
   theme: {
-    extend: {
-      fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans]
-      },
-      container: {
-        padding: {
-          DEFAULT: '1rem',
-          sm: '2rem',
-          lg: '4rem',
-          xl: '5rem',
-          '2xl': '8rem'
-        }
-      }
-    }
+    extend: {},
   },
-  plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/typography')
-  ]
+  plugins: [],
 }
